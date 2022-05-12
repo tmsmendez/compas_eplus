9cc5faa0140bae49477a62ed32af2295770b1f05
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2022-05-11 22:46:52 -0700

    read, docstrings

---
+++ src/compas_eplus/read_write/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+__author__ = ["Tomas Mendez Echenagucia"]
+__copyright__ = "Tomas Mendez Echenagucia - University of Washington"
+__license__ = "MIT License"
+__email__ = "tmendeze@uw.edu"
+__version__ = "0.1.0"
+
+from .read import *
+# from .write import *
---
+++ src/compas_eplus/read_write/read.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+
+__author__ = ["Tomas Mendez Echenagucia"]
+__copyright__ = "Tomas Mendez Echenagucia - University of Washington"
+__license__ = "MIT License"
+__email__ = "tmendeze@uw.edu"
+__version__ = "0.1.0"
+
+
+def read_mean_zone_temperatures(building, filepath):
+    """
+    Reads mean zone air temperatures from an Energy+ result file. 
+
+    Parameters
+    ----------
+    building: object
+        The building object where results will be stored
+    filepath: str
+        Path to the energy+ result file
+    
+    Return
+    ------
+    dict
+        The resulting air temperatures per time key and zone
+    list, 
+        A list of times for each temperature measurement. Each item is a list
+        with the structure: of hour, day, month 
+    
+    """
+    fh = open(filepath, 'r')
+    lines = fh.readlines()
+    fh.close()
+    temps = {}
+    times = []
+    num_zones = len(building.zones)
+    num_intro = 9 + num_zones
+    del lines[:num_intro]
+    del lines[-2:]
+    counter = 0
+    for i in range(0, len(lines), num_zones + 1):
+        temps[counter] = {}
+        line = lines[i]
+        time = line.split(',')
+        for j in range(num_zones):
+            line = lines[i + j + 1]
+            _, temp = line.split(',')
+            temps[counter][j] = float(temp)
+        
+        month = int(time[2])
+        day = int(time[3])
+        hour = int(time[5]) - 1
+        times.append([hour, day, month])
+        counter += 1
+    mean_air_temperatures = temps
+    result_times = times
+    return mean_air_temperatures, result_times
